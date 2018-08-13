b379ba7be5873ea8997789444fa005ebe7197d84
vinicius <vigusmao@gmail.com>
2018-08-13 10:52:43 -0300

    Aula 13/08

--- NumeroFaltando/src/Principal.java
+++ NumeroFaltando/src/Principal.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Random;
+
 public class Principal {
 
     /**
@@ -7,9 +10,8 @@ public class Principal {
      *
      * @param numeros um array de inteiros distintos
      * @return o inteiro ausente;
-     *         ou -1 caso haja mais de um inteiro ausente
-     *         ou a lista esteja vazia
-     *
+     * ou -1 caso haja mais de um inteiro ausente
+     * ou a lista esteja vazia
      */
     static int encontrarNumeroAusente(int[] numeros) {
         int n = numeros.length;
@@ -38,4 +40,130 @@ public class Principal {
 
         return numeroAusente;
     }
+
+    static int encontrarNumeroAusenteOrdenando(int[] numeros) {
+        int n = numeros.length;
+        if (n == 0) {
+            return -1;  // lista vazia
+        }
+
+        Arrays.sort(numeros);
+
+        int numeroAusente = -1;
+
+        int pos = 0;
+        for (int candidatoDaVez = 0; candidatoDaVez <= n; candidatoDaVez++) {
+
+            int numeroEncontrado = pos < n ? numeros[pos] : n + 1;
+            if (numeroEncontrado == candidatoDaVez) {
+                pos++;
+            } else if (numeroEncontrado > candidatoDaVez) {
+                // encontrei um numero ausente!!!
+                if (numeroAusente != -1) {
+                    return -1;  // mais de um ausente!!!
+                } else {
+                    numeroAusente = candidatoDaVez;
+                }
+            } else {  // if (numeroEncontrado < candidatoDaVez) {
+                return -1;  // certamente haverá mais de um ausente!
+
+            }
+        }
+        return numeroAusente;
+    }
+
+    static int encontrarNumeroAusenteLinear(int[] numeros) {
+        int n = numeros.length;
+        if (n == 0) {
+            return -1;  // lista vazia
+        }
+
+        boolean[] marcadores = new boolean[n + 1];
+        for (int i = 0; i <= n; i++) {
+            marcadores[i] = false;
+        }
+
+//        for (int i = 0; i < n; i++) {
+//            int numeroEncontrado = numeros[i];
+//            marcadores[numeroEncontrado] = true;
+//        }
+//        Da maneira abaixo é um pouco mais elegante.
+        
+        for (int numeroEncontrado : numeros) {
+            marcadores[numeroEncontrado] = true;
+        }
+
+        int numeroAusente = -1;
+
+        for (int candidatoDaVez = 0; candidatoDaVez <= n; candidatoDaVez++) {
+            if (!marcadores[candidatoDaVez]) {
+                // encontrei um numero ausente!!!
+                if (numeroAusente != -1) {
+                    return -1;  // mais de um ausente!!!
+                } else {
+                    numeroAusente = candidatoDaVez;
+                }
+            }
+        }
+
+        return numeroAusente;
+    }
+
+    private static int[] createArray(int n) {
+        int[] numeros = new int[n];
+        int x = 0;
+        for (int pos = 0; pos < n; pos += 2) {
+            numeros[pos] = x++;
+        }
+        for (int pos = 1; pos < n; pos += 2) {
+            numeros[pos] = x++;
+        }
+
+        Random random = new Random();
+        int y = random.nextInt(n + 1);
+        if (y < n) {
+            numeros[y] = n;
+        }
+        return numeros;
+    }
+
+    private static void imprimirArray(int[] array) {
+//        for (int i = 0; i < array.length; i++) {
+//            System.out.print(array[i] + " ");
+//        }
+//        A linha abaixo faz a mesma coisa, melhor.
+
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static void main(String[] args) {
+        for (int n = 1_000; n < 1_000_000; n *= 2) {
+            System.out.println("\n---------------\nn = " + n);
+
+            int[] numeros = createArray(n);
+//            imprimirArray(numeros);
+
+            long inicio, duracao;
+            int resultado;
+
+            inicio = System.currentTimeMillis();
+            resultado = encontrarNumeroAusente(numeros);
+            duracao = System.currentTimeMillis() - inicio;
+            System.out.printf("O número ausente é %d (duração: %d milis) -- O(n^2)\n",
+                    resultado, duracao);
+
+            inicio = System.currentTimeMillis();
+            resultado = encontrarNumeroAusenteOrdenando(numeros);
+            duracao = System.currentTimeMillis() - inicio;
+            System.out.printf("O número ausente é %d (duração: %d milis) -- O(n log n)\n",
+                    resultado, duracao);
+
+            inicio = System.currentTimeMillis();
+            resultado = encontrarNumeroAusenteLinear(numeros);
+            duracao = System.currentTimeMillis() - inicio;
+            System.out.printf("O número ausente é %d (duração: %d milis) -- O(n)\n",
+                    resultado, duracao);
+        }
+    }
 }
+
--- NumeroFaltando/src/PrincipalTest.java
+++ NumeroFaltando/src/PrincipalTest.java
@@ -5,17 +5,22 @@ public class PrincipalTest {
 
     @Test
     public void encontrarNumeroAusenteTest() {
-/*        int[] arrayDeTeste = new int[4];
-          arrayDeTeste[0] = 0;
-          arrayDeTeste[1] = 4;
-          arrayDeTeste[2] = 1;
-          arrayDeTeste[3] = 3;
-*/
-        // A linha seguinte é equivalente ao bloco comentado acima.
-        int[] arrayDeTeste = new int[] {0, 4, 1, 3};
-
         Assert.assertEquals("O número ausente não foi encontrado",
-                2, Principal.encontrarNumeroAusente(arrayDeTeste));
+                2, Principal.encontrarNumeroAusente(new int[] {0, 4, 1, 3}));
+        Assert.assertEquals("O número ausente não foi encontrado",
+                2, Principal.encontrarNumeroAusenteOrdenando(new int[] {0, 4, 1, 3}));
+        Assert.assertEquals("O número ausente não foi encontrado",
+                2, Principal.encontrarNumeroAusenteLinear(new int[] {0, 4, 1, 3}));
+    }
+
+    @Test
+    public void encontrarNumeroAusenteTestParaElementoAusenteExtremo() {
+        Assert.assertEquals("O número ausente não foi encontrado",
+                0, Principal.encontrarNumeroAusenteOrdenando(new int[] {1, 2, 3, 4}));
+        Assert.assertEquals("O número ausente não foi encontrado",
+                4, Principal.encontrarNumeroAusenteOrdenando(new int[] {0, 1, 2, 3}));
+        Assert.assertEquals("O número ausente não foi encontrado",
+                4, Principal.encontrarNumeroAusenteLinear(new int[] {0, 1, 2, 3}));
     }
 
     @Test
@@ -23,6 +28,10 @@ public class PrincipalTest {
         int[] arrayVazio = new int[] {};
         Assert.assertEquals("O valor retornado deve ser -1 para lista vazia",
                 -1, Principal.encontrarNumeroAusente(arrayVazio));
+        Assert.assertEquals("O valor retornado deve ser -1 para lista vazia",
+                -1, Principal.encontrarNumeroAusenteOrdenando(arrayVazio));
+        Assert.assertEquals("O valor retornado deve ser -1 para lista vazia",
+                -1, Principal.encontrarNumeroAusenteLinear(arrayVazio));
     }
 
     @Test
@@ -31,5 +40,11 @@ public class PrincipalTest {
         Assert.assertEquals("O valor retornado deve ser -1 para lista " +
                 "com elementos repetidos",
                 -1, Principal.encontrarNumeroAusente(arrayComRepeticao));
+        Assert.assertEquals("O valor retornado deve ser -1 para lista " +
+                        "com elementos repetidos",
+                -1, Principal.encontrarNumeroAusenteOrdenando(arrayComRepeticao));
+        Assert.assertEquals("O valor retornado deve ser -1 para lista " +
+                        "com elementos repetidos",
+                -1, Principal.encontrarNumeroAusenteLinear(arrayComRepeticao));
     }
 }
