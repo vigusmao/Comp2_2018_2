74ab548a0df45373b5362b8d262bf18feac95ad2
vinicius <vigusmao@gmail.com>
2018-10-05 10:25:04 -0300

    Aula 05/10

--- Banco/src/Banco.java
+++ Banco/src/Banco.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class Banco {
 
@@ -46,49 +44,41 @@ public class Banco {
         Random random = new Random();
         Banco bancoUnico = new Banco();
         Pessoa magnata = new PessoaFisica(12345);
-        for (int i = 0; i < 64_000; i++) {
+        for (int i = 0; i < 1_000_000; i++) {
             Conta novaConta = bancoUnico.abrirConta(magnata);
             long valorAleatorio = Math.abs(random.nextLong());
             novaConta.depositar(valorAleatorio);
         }
-        Conta umaConta = bancoUnico.getConta(222);
-        System.out.printf("Saldo da conta 222 = %.2f\n",
-                umaConta.getSaldo());
-        umaConta.depositar(500);
-        System.out.printf("Saldo da conta 222 = %.2f\n",
-                umaConta.getSaldo());
 
         // Vamos descobrir se há duas contas com o mesmo saldo
         long inicio = System.currentTimeMillis();
         int nContas = bancoUnico.contasCorrentes.size();
+
+        Map<Double, Conta> contaBySaldo = new HashMap<>();
         boolean encontrei = false;
+
         for (int i = 0; i < nContas; i++) {
             Conta conta = bancoUnico.contasCorrentes.get(i);
             double saldo = conta.getSaldo();
-            for (int j = i+1; j < nContas; j++) {
-                Conta outraConta = bancoUnico.contasCorrentes.get(j);
-                if (outraConta.getSaldo() == saldo) {
-                    // encontrei!!!!!
-                    System.out.printf("Encontrei! As contas " +
+            Conta contaJaVista = contaBySaldo.get(saldo);
+            if (contaJaVista != null) {
+                // ja existia uma conta com esse mesmo saldo!!!!!!!
+                System.out.printf("Encontrei! As contas " +
                             "%d e %d têm ambas o saldo %.2f\n",
                             conta.getNumeroDaConta(),
-                            outraConta.getNumeroDaConta(),
+                            contaJaVista.getNumeroDaConta(),
                             saldo);
-                    encontrei = true;
-                    break;
-                }
-            }
-            if (encontrei) {
+                encontrei = true;
                 break;
+            } else {
+                contaBySaldo.put(saldo, conta);
             }
         }
+
         if (!encontrei) {
             System.out.println("Não encontrei!");
         }
         long duracao = System.currentTimeMillis() - inicio;
         System.out.printf("Duracao = %d milissegundos\n", duracao);
-
-
-        // Como fazer isso de um jeito MUITO mais eficiente?????
     }
 }
--- Banco/src/PessoaFisica.java
+++ Banco/src/PessoaFisica.java
@@ -1,3 +1,7 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 public class PessoaFisica extends Pessoa {
 
     private long cpf;
@@ -11,5 +15,33 @@ public class PessoaFisica extends Pessoa {
     @Override
     public long getId() {
         return this.cpf;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PessoaFisica that = (PessoaFisica) o;
+        return cpf == that.cpf;
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(cpf);
+    }
+
+    public static void main(String[] args) {
+        Map<PessoaFisica, Long> telefoneByPessoa;
+        telefoneByPessoa = new HashMap<>();
+
+        PessoaFisica fulano = new PessoaFisica(1234);
+        telefoneByPessoa.put(fulano, 99999999L);
+
+        PessoaFisica outraInstanciaDeFulano = new PessoaFisica(1234);
+
+        System.out.println("O telefone do fulano é " +
+            telefoneByPessoa.get(outraInstanciaDeFulano));
+
     }
 }
