146e40703f9be2b8db74b196c0489e347006a805
vinicius <vinicius@hypermindr.com>
2018-11-07 08:09:07 -0300

    LAB9

--- MediaNotas/src/CalculadoraMedias.java
+++ MediaNotas/src/CalculadoraMedias.java
@@ -7,7 +7,7 @@ public class CalculadoraMedias {
     private static float calcularMediaDaTurma(String nomeDoArquivo)
             throws FileNotFoundException {
 
-        File f = new File(nomeDoArquivo);;
+        File f = new File(nomeDoArquivo);
         Scanner sc = null;
 
         try {
@@ -22,12 +22,16 @@ public class CalculadoraMedias {
                 try {
                     float nota = Float.parseFloat(linha);
 
+                    // ToDo processar nota lida
+
                 } catch (NumberFormatException e) {
 
+                    // ToDo incrementar contador de inválidas
                 }
             }
 
             if (quantLinhasInvalidas > quantNotas) {
+                // ToDo criar a classe da exceção abaixo
                 throw new ArquivoCorrompidoException(
                         String.format("O arquivo tem " +
                             "%d linhas inválidas", quantLinhasInvalidas));
@@ -43,8 +47,16 @@ public class CalculadoraMedias {
 
 
     public static void main(String[] args) {
+
+        // ToDo envolver as próximas duas linhas num loop
+        // ToDo criar o método que lê do teclado o nome do arquivo
         String nomeArquivo = lerNomeArquivoDoTeclado();
         float media = calcularMediaDaTurma(nomeArquivo);
+        // ToDo capturar FileNotFoundException e se manter no loop
+        // ToDo sair do loop por uma ArquivoCorrompidoException
+        // ToDo sair do loop se a média foi obtida corretamente
+
         System.out.printf("Media = %.2f\n", media);
+        // ToDo se houve ArquivoCorrompidoException, mostrar msg
     }
 }
